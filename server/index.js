cc28e0dd61a558014c2825381a98c3b607b746fc
niyatipandey <niyatipm25@gmail.com>
2026-07-16 00:03:23 +0530

    Allow Vercel frontend and Backend render

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://layrd-eight.vercel.app"
   ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
 }));
