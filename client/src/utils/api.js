eb39b4c98f86eb355df7d991b1782c263b7fb3ab
niyatipandey <niyatipm25@gmail.com>
2026-07-19 08:59:05 +0530

    fixed the backend route

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://layrd-backend.onrender.com";
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://layrd-backend.onrender.com";
+
 export const getJsonRequest = ()=>({
     'Content-Type':'application/json'
 })
